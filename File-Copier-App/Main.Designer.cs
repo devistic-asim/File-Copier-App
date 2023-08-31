02209941e8c8a8ef22393f21ba1dd1be4a3b98db
Muhammad Asim <asimkamal251@gmail.com>
2023-08-31 17:38:35 +0500

    Main file updated

---
+++ File-Copier-App/Main.Designer.cs
@@ -0,0 +1,65 @@
+﻿
+namespace File_Copier_App
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Heading = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Heading
+            // 
+            this.Heading.AutoSize = true;
+            this.Heading.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.488F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Heading.Location = new System.Drawing.Point(209, 27);
+            this.Heading.Name = "Heading";
+            this.Heading.Size = new System.Drawing.Size(174, 26);
+            this.Heading.TabIndex = 0;
+            this.Heading.Text = "File Copier Form";
+            this.Heading.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(601, 671);
+            this.Controls.Add(this.Heading);
+            this.Name = "Main";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Heading;
+    }
+}
+
--- File-Copier-App/Main.cs
+++ File-Copier-App/Main.cs
@@ -10,11 +10,19 @@ using System.Windows.Forms;
 
 namespace File_Copier_App
 {
-    public partial class Form1 : Form
+    public partial class Main : Form
     {
-        public Form1()
+        public Main()
         {
             InitializeComponent();
+
+            // Set the form's start position to CenterScreen
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
